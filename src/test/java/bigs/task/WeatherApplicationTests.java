aaddd9766eb5a039db0b5badeff621f1aac2f465
leejinsol234 <desirable7@naver.com>
2024-05-03 15:57:04 +0900

    initial commit

---
+++ src/test/java/bigs/task/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package bigs.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
